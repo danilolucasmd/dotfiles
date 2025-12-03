a6f690c441520fb57283350bd82f29a438aab6b5
Danilo Dias <ddias@brex.com>
2025-12-03 11:06:35 -0300

    fix: nvim dotfiles

---
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,6 @@
+return {
+  "github/copilot.vim",
+  cmd = "Copilot", -- optional: lazy load on :Copilot command
+  event = "InsertEnter", -- optional: load when entering insert mode
+  config = function() end,
+}
