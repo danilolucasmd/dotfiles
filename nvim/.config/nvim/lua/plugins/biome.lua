ca9295ca17161cf5c5d34a047cc0a2683915d9ac
Danilo Dias <ddias@brex.com>
2025-12-03 14:36:29 -0300

    fix: add biome as the default formatter

---
+++ nvim/.config/nvim/lua/plugins/biome.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        javascript = { "biome" },
+        typescript = { "biome" },
+        javascriptreact = { "biome" },
+        typescriptreact = { "biome" },
+        json = { "biome" },
+        jsonc = { "biome" },
+        css = { "biome" },
+        html = { "biome" },
+        markdown = { "biome" },
+      },
+      format_on_save = true,
+    },
+  },
+}
