b6a98fde8336056655a9008facb5127e194f0d1c
Danilo de Lucas <danilolucasmd@gmail.com>
2025-12-17 16:23:32 -0300

    feat: update list of packages to install

--- install.sh
+++ install.sh
@@ -2,14 +2,25 @@
 #
 ### Teminal
 #
+# kitty
 # git (maybe more stuff to add yay after)
 # yay
 # stow
 # nvim
 # lazygit
+# nvm
+# node (through nvm)
+# tree-sitter-cli (globally through npm)
+# fzf
+# ripgrep
+# fd
+#
 # bluetui
 # wiremix
 #
+# hyprpaper
+#
 ### Applications
 #
+# 1password
 # brave-bin
