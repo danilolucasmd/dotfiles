2993f36f4fb3ef2dd552f7ff8b59b584b562ffcb
Danilo Dias <ddias@brex.com>
2026-04-01 16:11:23 -0300

    feat: disable arrow keys on neovim

--- nvim2/.config/nvim/lua/config/keymaps.lua
+++ nvim2/.config/nvim/lua/config/keymaps.lua
@@ -13,6 +13,18 @@ vim.keymap.set("i", "<up>", "<nop>", { noremap = true })
 vim.keymap.set("i", "<down>", "<nop>", { noremap = true })
 vim.opt.mouse = ""
 
+-- Disable Arrow Keys in Normal, Visual, and Operator-pending modes
+vim.keymap.set("", "<Up>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("", "<Down>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("", "<Left>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("", "<Right>", "<Nop>", { noremap = true, silent = true })
+
+-- Disable Arrow Keys in Insert mode
+vim.keymap.set("i", "<Up>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("i", "<Down>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("i", "<Left>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("i", "<Right>", "<Nop>", { noremap = true, silent = true })
+
 -- Move buffers
 vim.keymap.set("n", "<leader>bH", "<cmd>BufferLineMovePrev<CR>", { desc = "Move buffer left" })
 vim.keymap.set("n", "<leader>bL", "<cmd>BufferLineMoveNext<CR>", { desc = "Move buffer right" })
