83aec9fef306cd8fdcfcd1f87cdc1d9be11ef085
Danilo de Lucas <danilolucasmd@gmail.com>
2026-06-02 10:49:48 -0300

    fix: add recording by region + waybar indicator

--- scripts/screen-record.sh
+++ scripts/screen-record.sh
@@ -2,5 +2,7 @@
 
 pgrep -x "wf-recorder" && pkill -INT -x wf-recorder && exit 0
 
+geometry=$(slurp) || exit 0
+
 dateTime=$(date +%m-%d-%Y-%H:%M:%S)
-wf-recorder --bframes max_b_frames -f $HOME/Videos/$dateTime.mp4
+wf-recorder -g "$geometry" --bframes max_b_frames -f $HOME/Videos/$dateTime.mp4
